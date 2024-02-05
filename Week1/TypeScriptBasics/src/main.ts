74af21ec48a7e84f248e7b3cdae0070946bdbbfb
Sebbedeb <zor.seb.0901@gmail.com>
2024-02-05 15:59:51 +0100

    assignment 1

--- Week1/TypeScriptBasics/src/main.ts
+++ Week1/TypeScriptBasics/src/main.ts
@@ -136,3 +136,224 @@ function firstElement<T>(input: T[]): T {
 function combine<T extends object, U extends object>(obj1: T, obj2: U): T & U {
   return { ...obj1, ...obj2 };
 }
+
+/*9. Array Types:
+Create an array of numbers. But instead of using the number type, use the array type.
+Create a Multidimensional Array of strings to use for the game Tic Tac Toe. The array should have 3 rows and 3 columns. All string values should be "-".
+*/
+
+let numbers: Array<number> = [1, 2, 3];
+let ticTacToe: Array<Array<string>> = [
+  ["-", "-", "-"],
+  ["-", "-", "-"],
+  ["-", "-", "-"]
+];
+
+//10. Exclamation Mark: Where in code below would you need to use the exclamation mark to tell TypeScript that a variable is not null or undefined?
+
+ // Part 1
+ // A variable that might be null or undefined
+ let nullableValue: string | null | undefined = "Hello";
+
+ // Use the exclamation mark to assert that the value is non-null
+ let nonNullableValue: string = nullableValue!;
+
+ console.log(nonNullableValue); // Output: Hello
+
+
+ function possibleUndefinedStringFunction(): string | undefined {
+  return "I could theoretically be undefined.";
+ }
+
+// Part 2
+// A variable that might be null or undefined
+let myString: string | undefined = possibleUndefinedStringFunction();
+// Use the exclamation mark to assert that the value is non-null
+let lemgth: number = myString!.length;
+
+//11. Question mark: Where in code below would you need to use the question mark to tell TypeScript that a variable is optional?
+
+// Part 1
+// A function that takes an optional parameter
+function printName(name?: string) {
+  console.log(name);
+}
+
+// Call the function without a parameter
+printName(); // Output: undefined
+// Call the function with a parameter
+printName("John"); // Output: John
+
+// Part 2
+// A type alias with an optional age property
+type Person2 = {
+  name: string;
+  age?: number;
+};
+
+// Create a person object with an age property
+// Create a person object without an age property
+
+let personWithoutAge: Person2 = { name: "John" };
+let personWithAge: Person2 = { name: "John", age: 30 };
+
+/*12. Unions - Narrowing the Type: 
+Write a function that takes a string or number as a parameter. If the parameter is a string, return the string. 
+If the parameter is a number, return the number multiplied by 2. (use the typeof operator to check the type of the parameter)
+*/
+
+type StringOrNumberType = string | number;
+
+function numberOrStringFunction(input: StringOrNumberType): string | number {
+  {
+    if (typeof input === "string") {
+      return input;
+    }
+    else {
+      return input * 2;
+    }
+  }
+}
+
+/* 13. Type Assertion
+Use type assertion to convert a variable from type any to type string. Use the as keyword and the angle-bracket syntax.
+Call a div element with id="myDiv" and convert it to type HTMLInputElement.
+*/
+
+
+let anyVar3: any = "hello";
+let anyVarAsString: string = anyVar3 as string;
+
+let divAsHTMLInputElement: HTMLInputElement = document.getElementById("myDiv") as HTMLInputElement;
+
+/* 14. Literal Types combined with Union Types
+Create a function that takes a string called direction as a parameter. Use literal types to narrow the type of the parameter.
+If the direction is "left" return 1, if the direction is "right" return 2, if the direction is "up" return 3, 
+if the direction is "down" return 4. (use a switch statement)
+*/
+
+function whichWay(direction: "left" | "right" | "up" | "down"){
+  switch(direction){
+    case "left" : return 1;
+    case "right" : return 2;
+    case "up" : return 3;
+    case "down" : return 4;
+  }
+}
+
+/* 15. in Operator Narrowing
+    create two simple type aliases for a human and an alien. Both alias should have a specific function like eat and fly.
+    create a function that takes in a creator parameter that could be either a person or an alien. 
+    Use the in operator to narrow the type of the parameter. 
+    When you use the in operator you have to use the function to check if the property exists on the object. 
+    If the parameter is a human, return the function belonging to the human otherwise return the function belonging to the alien.
+*/
+
+type Human = {
+  eat: () => void;
+};
+
+type Alien = {
+  fly: () => void;
+};
+
+function createBeing(being: Human | Alien) : (() => void)
+{
+  if("eat" in being){
+    return being.eat;
+  }
+  else{
+    return being.fly;
+  }
+}
+
+/* 16. instanceof Operator Narrowing
+  create two simple objects for a person and a car.
+  create a function that takes a person or a car as a parameter. 
+  Use the instanceof operator to narrow the type of the parameter. If the parameter is a person, return the name property. 
+  If the parameter is a car, return the model property.
+*/
+
+class PersonObject {
+  name: string;
+  constructor(name: string){
+    this.name = name;
+  }
+}
+
+class CarObject {
+  model: string;
+  constructor(model: string){
+    this.model = model;
+  }
+}
+
+function personOrCar(being: PersonObject | CarObject) : string
+{
+  if(being instanceof PersonObject){
+    return being.name;
+  }
+  else{
+    return being.model;
+  }
+}
+
+// 17. type Predicates
+
+interface Bird {
+  fly(): void;
+  layEggs(): void;
+}
+
+interface Fish {
+  swim(): void;
+  layEggs(): void;
+}
+
+// write a type predicate to narrow the type of the fish parameter
+
+function isFish(animal: Fish | Bird): animal is Fish {
+  return (animal as Fish).swim !== undefined;
+}
+
+function howToMove(animal: Fish | Bird) {
+  if (isFish(animal)) {
+    animal.swim();
+  } else {
+    animal.fly();
+  }
+}
+
+
+/* 18. Index Signatures
+  Create an interface for a person with a name property and an index signature that allows the interface to have additional properties.
+  Create an object of type person with a name property and an additional property called age.
+*/
+
+interface PersonInterface {
+  name: string;
+  [propName: string]: any;
+}
+
+let personObjectWithAge: PersonInterface = {
+  name: "Johnny",
+  age: 30
+};
+
+/* 19. Intersection Types
+  Create two interfaces, one for a person and one for a student. 
+  The person interface should have a name property and the student interface should have a studentId property.
+  Create a function that takes a person and a student as parameters. 
+  Use intersection types to combine the two interfaces into one. The function should return an object with the properties from both interfaces.
+*/
+
+interface PersonInterface2 {
+  name: string;
+}
+interface StudentInterface {
+  studentId: number;
+}
+
+function interSectionAndStuff(person: PersonInterface2, student: StudentInterface): PersonInterface2 & StudentInterface {
+  return { ...person, ...student};
+}
